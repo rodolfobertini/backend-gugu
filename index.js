414dc6f72e15d1c56865ae4dd31a3a97287ae3f5
Rodolfo Bertini <132242813+rodolfobertini@users.noreply.github.com>
2024-01-26 09:44:52 -0300

    commonjs to type module nodejs

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var express = require('express')
-const app = express()
-const port = 3000
-var cors = require('cors')
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+const corsOptions = {
+ origin: '*',
+ optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 app.get('/', cors(corsOptions), (req, res, next) => {
-    const json = JSON.stringify({
-        nome: req.query.nome,
-        idade: req.query.idade
-    })
-  res.send(json)
-})
+ const json = JSON.stringify({
+      nome: req.query.nome,
+      idade: req.query.idade
+ });
+ res.send(json);
+});
 
 app.listen(port, () => {
-  console.log(`Backend rodando no local e porta ${port}`)
-})
-
-var corsOptions = {
-  origin: 'http://example.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
+ console.log(`Backend rodando no local e porta ${port}`);
+});
